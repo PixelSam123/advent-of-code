96114b11520ae77f4adc6d5448a87b39e7cfee1e
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-12-08 13:41:50 +0700

    Add sliding window function for Sonar Sweep

--- python/sonar_sweep/sonar_sweep.py
+++ python/sonar_sweep/sonar_sweep.py
@@ -12,10 +12,30 @@ def sonar_sweep(input_filepath: str):
         )
 
 
+def sonar_sweep_windowed(input_filepath: str, window_size: int = 1):
+    assert window_size >= 1, "Sliding window size must be at least 1"
+
+    with open(input_filepath) as input:
+        input_numbers = [int(line.rstrip()) for line in input.readlines()]
+        input_groups = [
+            sum(window) / window_size
+            for window in zip(
+                *(input_numbers[start_idx:] for start_idx in range(window_size))
+            )
+        ]
+
+        return reduce(
+            lambda acc, curr_val: acc + 1 if curr_val[1] > curr_val[0] else acc,
+            zip(input_groups, input_groups[1:]),
+            0,
+        )
+
+
 def main():
     from os.path import dirname
 
     print(sonar_sweep(f"{dirname(__file__)}/input.txt"))
+    print(sonar_sweep_windowed(f"{dirname(__file__)}/input.txt", 3))
 
 
 if __name__ == "__main__":
