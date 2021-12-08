8b7547fc0fdd38be3b14e631e66ef75773531213
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-12-08 14:21:19 +0700

    Add second half of Dive

--- python/dive/dive.py
+++ python/dive/dive.py
@@ -25,10 +25,41 @@ def dive(input_filepath: str):
         return dist_x * depth
 
 
+def dive_aimed(input_filepath: str):
+    with open(input_filepath) as input:
+        aim = 0
+        dist_x = 0
+        depth = 0
+
+        def process_forward(x):
+            nonlocal dist_x
+            nonlocal depth
+            dist_x += x
+            depth += x * aim
+
+        def add_aim(θ):
+            nonlocal aim
+            aim += θ
+
+        def subtract_aim(θ):
+            nonlocal aim
+            aim -= θ
+
+        move_mapping = {"forward": process_forward, "down": add_aim, "up": subtract_aim}
+
+        for direction, distance_or_aim in (
+            line.rstrip().split(" ") for line in input.readlines()
+        ):
+            move_mapping[direction](int(distance_or_aim))
+
+        return dist_x * depth
+
+
 def main():
     from os.path import dirname
 
     print(dive(f"{dirname(__file__)}/input.txt"))
+    print(dive_aimed(f"{dirname(__file__)}/input.txt"))
 
 
 if __name__ == "__main__":
