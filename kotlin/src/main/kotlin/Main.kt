fd33da86c6c76502e66ca24f8ff84f12e1b8fff5
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-12-11 10:16:29 +0700

    Add first half of Dive (kotlin)

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -9,6 +9,26 @@ fun sonarSweep(inputFilepath: String): Int {
     }
 }
 
+fun dive(inputFilepath: String): Int {
+    val inputNumbers = File(inputFilepath).readLines().map { line -> line.split(" ") }
+
+    var distX = 0
+    var depth = 0
+
+    val moveMapping = mapOf(
+        "forward" to { x: Int -> distX += x },
+        "down" to { y: Int -> depth += y },
+        "up" to { y: Int -> depth -= y }
+    )
+
+    inputNumbers.forEach { (direction, distance) ->
+        moveMapping[direction]?.invoke(distance.toInt())
+    }
+
+    return distX * depth
+}
+
 fun main(args: Array<String>) {
     println(sonarSweep("${System.getProperty("user.dir")}/../input/sonar_sweep.txt"))
+    println(dive("${System.getProperty("user.dir")}/../input/dive.txt"))
 }
